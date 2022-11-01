68b3d080ed2d9267d901cd657ec50aab2b2a44e8
klibin <km.putnam2@gmail.com>
2022-11-01 14:27:06 -0600

    turned characters query into custom hook

--- src/pages/CharactersList.js
+++ src/pages/CharactersList.js
@@ -1,32 +1,23 @@
 import React from 'react'
-import {useQuery, gql} from '@apollo/client'
+import { Link } from 'react-router-dom';
+import { useCharacters } from '../hooks/useCharacters';
 import './CharacterList.css'
-const GET_CHARACTERS = gql `
-query {
-    characters {
-      results {
-        name
-        id
-        image
-      }
-    }
-  }
-`
+
 
 export default function CharactersList() {
-
-    const {error, data, loading} = useQuery(GET_CHARACTERS);
-        console.log(data)
+const {error, loading, data} = useCharacters();
 
     if(loading) return <div>spinner...</div>
     if(error) return <div>something went wrong</div>
   return (
     <div className= "character-list">
         {data.characters.results.map(char => (
+            <Link to={`/${char.id}`}>
             <div key={char.id}>
                 <img src={char.image}/>
                 <h2>{char.name}</h2>
             </div>
+            </Link>
         ))}
     </div>
   )
