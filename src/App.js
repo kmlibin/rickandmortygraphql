ede91d4226b2ee9ac5a93478089dfb7615f7597f
klibin <km.putnam2@gmail.com>
2022-11-03 10:29:31 -0600

    added a search page

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import CharactersList from "./pages/CharactersList";
 import Character from "./pages/Character";
+import Search from "./pages/Search";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<CharactersList />} />
+        <Route path= "/search" element={<Search />} />
         <Route path ="/:id" element={<Character />} />
       </Routes>
     </div>
---
+++ src/pages/Search.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import {gql, useLazyQuery} from '@apollo/client'
+
+const GET_CHARACTER_LOCATIONS = gql `
+query GetCharacterLocations($name: String!) {
+    characters(filter: {
+      name: $name
+    }) {
+      results {
+        name
+        location {
+          name
+          type
+        }
+        origin{
+          dimension
+        }
+        
+      }
+    }
+  }
+`
+
+export default function Search() {
+  const [name, setName] = useState("");
+  //first is a functino we invoke to execute query, second is an object with loading, error, data. also get "called", which just checks if the func has ever been called. bool.
+  const [getLocations, {loading, error, data, called} ] = useLazyQuery(GET_CHARACTER_LOCATIONS, {
+        variables: {name}
+  })
+  return (
+    <div>
+      <input
+        value={name}
+        type="text"
+        onChange={(e) => {
+          setName(e.target.value);
+        }}
+      />
+      <button onClick= {() => getLocations()}>Search</button>
+      {loading && <div>spinner here</div>}
+      {error && <div>something went wrong</div>}
+      {data && (
+        <ul>
+            {data.characters.results.map(char => (
+                <li key={char.location.name}>{char.location.name}</li>
+            ))}
+        </ul>
+      )}
+    </div>
+  );
+}
